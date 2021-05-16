2a3375a86e8a2fb79f388999b92b864ba20e9964
kobi <73976733+KobiSaada@users.noreply.github.com>
2021-05-16 18:38:46 +0300

    Update Virologist.cpp

--- sources/Virologist.cpp
+++ sources/Virologist.cpp
@@ -6,12 +6,12 @@ using namespace pandemic;
 Player &Virologist::treat(City c)
 {       if (_Curr_city != c) {
           if(count(_cards.begin(), _cards.end(), c) == 0) {
-            throw std::invalid_argument{"illegal action: you have no card of city " + city_string(c)};
+            throw std::invalid_argument{" you have no card of city " + city_string(c)};
         }
       _cards.erase(remove(_cards.begin(), _cards.end(), c), _cards.end());;
     }
     else if (board[c] == 0) {
-        throw std::invalid_argument{"illegal action: no more cubes remain in city " + city_string(c)};
+        throw std::invalid_argument{" no more cubes remain in city " + city_string(c)};
     }
     else if (board.have_cure(c)) {
         board[c] = 0;
